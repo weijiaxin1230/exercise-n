a82eeaa0dbc4c259a279c7f9ded36317bc55cf7d
weijiaxin1230 <1328341529@qq.com>
2018-08-14 16:51:35 +0800

    vuecli3

---
+++ vuecli3/src/views/Mine.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="about">
+        <h1>欢迎来到我的页面</h1>
+        <router-view/>
+        <br>
+        <router-link :to="{name:'test1',params:{name:'zs',age:45}}">test1</router-link> |
+        <router-link to="/test2/lisi/24">test2</router-link>
+    </div>
+</template>
+<script>
+    export default[
+
+    ]
+</script>
---
+++ vuecli3/src/views/Test2.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="about">
+        <h1>我是test2</h1>
+        姓名：{{$route.params.name}}
+        年龄：{{$route.params.age}}
+    </div>
+</template>
+<script>
+    export default[
+
+    ]
+</script>
